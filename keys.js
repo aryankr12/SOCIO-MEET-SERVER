cfad84d0c8c963f2788696b09d126dc6ec237f82
aryankr12 <53533353+aryankr12@users.noreply.github.com>
2021-01-12 15:35:57 +0530

    changed mongo URI
    ADD your own exiting uri for it

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    mongo_url : "mongodb+srv://uttam_kr:gBqsrJ2bx7eN0s6S@cluster0.vqenh.mongodb.net/uttam_kr?retryWrites=true&w=majority",
+    mongo_url : "mongodb+srv://<DB_USERNAME>:gBqsrJ2bx7eN0s6S@cluster0.vqenh.mongodb.net/<DB_USERNAME>?retryWrites=true&w=majority",
     jwt_source_key  : "Nevermind@1"//any random string.
 }
